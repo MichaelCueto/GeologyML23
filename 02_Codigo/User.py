4e6d7650588ce8f0dda045f563780096012d7fa1
MichaelCueto <michael.cueto.m@uni.pe>
2024-01-14 18:43:31 -0500

    Update

--- 02_Codigo/User.py
+++ 02_Codigo/User.py
@@ -7,8 +7,8 @@ class user_class():
         pass
 
     def Generate_Model(self,root):
-        root_model_bwi = '../04_Model/Hardness/BWI/BWI_1ley/'
-        root_model_spi = '../04_Model/Hardness/SPI/SPI_1ley/'
+        root_model_bwi = '../04_Model/Hardness/BWI/'
+        root_model_spi = '../04_Model/Hardness/SPI/'
         GeologyML(target='BWI',element='Na%',ley=1).RF_Model(root,root_model_bwi)
         GeologyML(target='SPI',element='Na%',ley=1).RF_Model(root,root_model_spi)
        
@@ -16,7 +16,7 @@ class user_class():
     def Generate_Data(self,root,root_bbdd):
         target1 = 'BWI'
         target2 = 'SPI'
-        root_model_bwi = '../04_Model/Hardness/BWI/BWI_1ley/'
-        root_model_spi = '../04_Model/Hardness/SPI/SPI_1ley/'
+        root_model_bwi = '../04_Model/Hardness/BWI/'
+        root_model_spi = '../04_Model/Hardness/SPI/'
         df = RF_predict(target1,target2,root,root_model_spi,root_model_bwi).SPI_Predict()
         df.to_excel(root_bbdd, index=False)
